13d2d2793fec575b8745132782ab50ec5a219699
Eldiiar Kudaiberdiev <eldiiar.kudaiberdiev@alatoo.edu.kg>
2022-05-26 00:29:04 +0600

    Initial commit

---
+++ PL2-Final/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PL2-Final
+//
+//  Created by Eldiiar on 26/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
